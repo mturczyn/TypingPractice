bf52748477a84398d434b9c724efb1630c6328a2
Michal Turczyn <turek1992@o2.pl>
2023-10-29 12:34:53 +0100

    Initial commit, current tasks: trying to do programatic app updates from whithin the app.

---
+++ TypingPractice/ApplicationDeployment.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypingPractice;
+
+public static class ApplicationDeploymentEnvVarNames
+{
+    public const string clickOncePrefix = "ClickOnce_";
+    public const string isNetworkDeployedEnvVar = $"{clickOncePrefix}IsNetworkDeployed";
+    public const string activationUriEnvVar = $"{clickOncePrefix}ActivationUri";
+    public const string currentVersionEnvVar = $"{clickOncePrefix}CurrentVersion";
+    public const string dataDirectoryEnvVar = $"{clickOncePrefix}DataDirectory";
+    public const string isFirstRunEnvVar = $"{clickOncePrefix}IsFirstRun";
+    public const string timeOfLastUpdateCheckEnvVar = $"{clickOncePrefix}TimeOfLastUpdateCheck";
+    public const string updatedApplicationFullNameEnvVar = $"{clickOncePrefix}UpdatedApplicationFullName";
+    public const string updatedVersionEnvVar = $"{clickOncePrefix}UpdatedVersion";
+    public const string updateLocationEnvVar = $"{clickOncePrefix}UpdateLocation";
+    public const string launcherVersionEnvVar = $"{clickOncePrefix}LauncherVersion";
+}
+
+/// <summary>
+/// Copied from DotNet Mage package:
+/// https://github.com/dotnet/deployment-tools/blob/main/Documentation/dotnet-mage/ApplicationDeployment.cs#L3
+/// </summary>
+public class ApplicationDeployment
+{
+
+    private static ApplicationDeployment currentDeployment = null;
+    private static bool currentDeploymentInitialized = false;
+
+    private static bool isNetworkDeployed = false;
+    private static bool isNetworkDeployedInitialized = false;
+
+    public static bool IsNetworkDeployed
+    {
+        get
+        {
+            if (!isNetworkDeployedInitialized)
+            {
+                bool.TryParse(Environment.GetEnvironmentVariable(ApplicationDeploymentEnvVarNames.isNetworkDeployedEnvVar), out ApplicationDeployment.isNetworkDeployed);
+                ApplicationDeployment.isNetworkDeployedInitialized = true;
+            }
+
+            return ApplicationDeployment.isNetworkDeployed;
+        }
+    }
+
+    public static ApplicationDeployment CurrentDeployment
+    {
+        get
+        {
+            if (!currentDeploymentInitialized)
+            {
+                ApplicationDeployment.currentDeployment = IsNetworkDeployed ? new ApplicationDeployment() : null;
+                ApplicationDeployment.currentDeploymentInitialized = true;
+            }
+
+            return ApplicationDeployment.currentDeployment;
+        }
+    }
+
+    public static Uri ActivationUri
+    {
+        get
+        {
+            Uri.TryCreate(Environment.GetEnvironmentVariable(ApplicationDeploymentEnvVarNames.activationUriEnvVar), UriKind.Absolute, out Uri val);
+            return val;
+        }
+    }
+
+    public static Version CurrentVersion
+    {
+        get
+        {
+            Version.TryParse(Environment.GetEnvironmentVariable(ApplicationDeploymentEnvVarNames.currentVersionEnvVar), out Version val);
+            return val;
+        }
+    }
+    public static string DataDirectory
+    {
+        get { return Environment.GetEnvironmentVariable(ApplicationDeploymentEnvVarNames.dataDirectoryEnvVar); }
+    }
+
+    public static bool IsFirstRun
+    {
+        get
+        {
+            bool.TryParse(Environment.GetEnvironmentVariable(ApplicationDeploymentEnvVarNames.isFirstRunEnvVar), out bool val);
+            return val;
+        }
+    }
+
+    public static DateTime TimeOfLastUpdateCheck
+    {
+        get
+        {
+            DateTime.TryParse(Environment.GetEnvironmentVariable(ApplicationDeploymentEnvVarNames.timeOfLastUpdateCheckEnvVar), out DateTime value);
+            return value;
+        }
+    }
+    public static string UpdatedApplicationFullName
+    {
+        get
+        {
+            return Environment.GetEnvironmentVariable(ApplicationDeploymentEnvVarNames.updatedApplicationFullNameEnvVar);
+        }
+    }
+
+    public static Version UpdatedVersion
+    {
+        get
+        {
+            Version.TryParse(Environment.GetEnvironmentVariable(ApplicationDeploymentEnvVarNames.updatedVersionEnvVar), out Version val);
+            return val;
+        }
+    }
+
+    public static Uri UpdateLocation
+    {
+        get
+        {
+            Uri.TryCreate(Environment.GetEnvironmentVariable(ApplicationDeploymentEnvVarNames.updateLocationEnvVar), UriKind.Absolute, out Uri val);
+            return val;
+        }
+    }
+
+    public static Version LauncherVersion
+    {
+        get
+        {
+
+            Version.TryParse(Environment.GetEnvironmentVariable(ApplicationDeploymentEnvVarNames.launcherVersionEnvVar), out Version val);
+            return val;
+        }
+    }
+
+    private ApplicationDeployment()
+    {
+        // As an alternative solution, we could initialize all properties here
+    }
+}
---
+++ TypingPractice/MainWindow.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System.Diagnostics;
+using System.Globalization;
+using System.Reflection;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Web;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+namespace TypingPractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly BitmapImage _piesioImage = new BitmapImage(new Uri(@"piesio.JPEG", UriKind.Relative));
+
+        private static char[] _letters = Enumerable.Range('a', 'z' - 'a').Select(x => (char)x).ToArray();
+        private static SpeechRecognitionEngine _recognizer;
+        private readonly SpeechSynthesizer _synthesizer;
+        private volatile bool _isSpeaking = false;
+
+        public static RoutedCommand UpdateCommand { get; } = new RoutedCommand();
+
+        public MainWindow()
+        {
+            // Create an in-process speech recognizer for the en-US locale.  
+            _recognizer = new SpeechRecognitionEngine(CultureInfo.CurrentCulture);
+
+            _synthesizer = new SpeechSynthesizer();
+            _synthesizer.SetOutputToDefaultAudioDevice();
+
+            InitializeComponent();
+
+            UpdateCommand.InputGestures.Add(new KeyGesture(Key.U, ModifierKeys.Control));
+
+            Loaded += MainWindow_Loaded;
+            Closing += MainWindow_Closing;
+            _synthesizer.SpeakCompleted += _synthesizer_SpeakCompleted;
+        }
+
+        private void _synthesizer_SpeakCompleted(object? sender, SpeakCompletedEventArgs e)
+        {
+            _isSpeaking = false;
+        }
+
+        private void MainWindow_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _recognizer?.Dispose();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+                "Installed speech recognizers: " +
+                string.Join(
+                    ',',
+                    SpeechRecognitionEngine.InstalledRecognizers()
+                        .Select(x => $"{x.Name} {x.Description}")));
+
+            if (false) StartSpeechRecognition();
+        }
+
+        private void TypedText_KeyDown(object sender, KeyEventArgs e)
+        {
+            var prevLetterToType = TextToType.Text;
+
+            var rnd = new Random();
+
+            var idx = rnd.Next(0, _letters.Length);
+
+            var nextLetter = char.ToUpper(_letters[idx]);
+
+            TextToType.Text = nextLetter.ToString();
+            var typedLetter = e.Key.ToString();
+            TypedText.Text = typedLetter;
+
+            if (!_isSpeaking)
+            {
+                _synthesizer.SpeakAsync(typedLetter);
+                _isSpeaking = true;
+            }
+
+            // Correct type
+            if (e.Key.ToString().ToLower() == prevLetterToType.ToString().ToLower())
+            {
+                var image = new Image()
+                {
+                    Source = _piesioImage,
+                    Height = 150,
+                    Margin = new Thickness(5),
+                };
+                image.MouseDown += (s, e) => piesie.Children.Clear();
+                piesie.Children.Add(image);
+            }
+        }
+
+        private static void StartSpeechRecognition()
+        {
+            // Create and load a dictation grammar.  
+            _recognizer.LoadGrammar(new DictationGrammar());
+
+            // Add a handler for the speech recognized event.  
+            _recognizer.SpeechRecognized +=
+              new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
+
+            // Configure input to the speech recognizer.  
+            _recognizer.SetInputToDefaultAudioDevice();
+
+            // Start asynchronous, continuous speech recognition.  
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        // Handle the SpeechRecognized event.  
+        static void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            MessageBox.Show("Recognized text: " + e.Result.Text);
+            Console.WriteLine("RECOGNIZED TEXT");
+            System.Diagnostics.Debug.WriteLine("RECOGNIZED TEXT");
+            Console.WriteLine("Recognized text: " + e.Result.Text);
+            System.Diagnostics.Debug.WriteLine("Recognized text: " + e.Result.Text);
+        }
+
+        private void CommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            var publisher_name = "TypingPractice";
+            var product_name = "TypingPractice";
+            var shortcutName = string.Concat(
+                Environment.GetFolderPath(Environment.SpecialFolder.Programs),
+                "\\",
+                publisher_name, 
+                "\\", 
+                product_name, 
+                ".appref-ms");
+            var process = new Process();
+            process.StartInfo = new ProcessStartInfo(shortcutName)
+            {
+                UseShellExecute = true,
+            };
+
+            process.Start();
+
+            Application.Current.Shutdown();
+
+            return;
+
+            var sb = new StringBuilder("App deploymewnt values");
+            sb.AppendLine();
+            var getters = typeof(ApplicationDeployment).GetProperties(BindingFlags.Static | BindingFlags.Public | BindingFlags.GetProperty);
+
+            foreach (var getter in getters)
+            {
+                var value = getter.GetValue(null);
+                sb.Append(getter.Name);
+                sb.Append(" = ");
+                sb.AppendLine($"{value}");
+            }
+            MessageBox.Show(sb.ToString());
+
+            if (!bool.TryParse(
+                Environment.GetEnvironmentVariable(ApplicationDeploymentEnvVarNames.isNetworkDeployedEnvVar),
+                out _))
+            {
+                MessageBox.Show($"Could not recognize {ApplicationDeploymentEnvVarNames.isNetworkDeployedEnvVar} environment variable");
+                return;
+            }
+
+            MessageBox.Show($"{ApplicationDeploymentEnvVarNames.isNetworkDeployedEnvVar} found, value = {ApplicationDeployment.IsNetworkDeployed}");
+
+            if (!ApplicationDeployment.IsNetworkDeployed)
+            {
+                MessageBox.Show($"{ApplicationDeploymentEnvVarNames.isNetworkDeployedEnvVar} was false");
+                return;
+            }
+
+            var activationUriRaw = Environment.GetEnvironmentVariable(ApplicationDeploymentEnvVarNames.activationUriEnvVar);
+            if (string.IsNullOrEmpty(activationUriRaw))
+            {
+                MessageBox.Show($"{ApplicationDeploymentEnvVarNames.activationUriEnvVar} environment variable was emtpy");
+                return;
+            }
+
+            var activationUri = new Uri(activationUriRaw);
+
+            var nvt = HttpUtility.ParseQueryString(activationUri.Query);
+            MessageBox.Show($"{ApplicationDeploymentEnvVarNames.activationUriEnvVar} environment variable successfully read, value = {activationUri}");
+        }
+    }
+}
