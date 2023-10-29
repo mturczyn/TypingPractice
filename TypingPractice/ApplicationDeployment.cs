034e9d5180659624c40013304cf452eaece5482f
Michal Turczyn <turek1992@o2.pl>
2023-10-29 12:36:19 +0100

    Code cleanup.

--- TypingPractice/ApplicationDeployment.cs
+++ TypingPractice/ApplicationDeployment.cs
@@ -6,21 +6,6 @@ using System.Threading.Tasks;
 
 namespace TypingPractice;
 
-public static class ApplicationDeploymentEnvVarNames
-{
-    public const string clickOncePrefix = "ClickOnce_";
-    public const string isNetworkDeployedEnvVar = $"{clickOncePrefix}IsNetworkDeployed";
-    public const string activationUriEnvVar = $"{clickOncePrefix}ActivationUri";
-    public const string currentVersionEnvVar = $"{clickOncePrefix}CurrentVersion";
-    public const string dataDirectoryEnvVar = $"{clickOncePrefix}DataDirectory";
-    public const string isFirstRunEnvVar = $"{clickOncePrefix}IsFirstRun";
-    public const string timeOfLastUpdateCheckEnvVar = $"{clickOncePrefix}TimeOfLastUpdateCheck";
-    public const string updatedApplicationFullNameEnvVar = $"{clickOncePrefix}UpdatedApplicationFullName";
-    public const string updatedVersionEnvVar = $"{clickOncePrefix}UpdatedVersion";
-    public const string updateLocationEnvVar = $"{clickOncePrefix}UpdateLocation";
-    public const string launcherVersionEnvVar = $"{clickOncePrefix}LauncherVersion";
-}
-
 /// <summary>
 /// Copied from DotNet Mage package:
 /// https://github.com/dotnet/deployment-tools/blob/main/Documentation/dotnet-mage/ApplicationDeployment.cs#L3
---
+++ TypingPractice/ApplicationDeploymentEnvVarNames.cs
@@ -0,0 +1,16 @@
+﻿namespace TypingPractice;
+
+public static class ApplicationDeploymentEnvVarNames
+{
+    public const string clickOncePrefix = "ClickOnce_";
+    public const string isNetworkDeployedEnvVar = $"{clickOncePrefix}IsNetworkDeployed";
+    public const string activationUriEnvVar = $"{clickOncePrefix}ActivationUri";
+    public const string currentVersionEnvVar = $"{clickOncePrefix}CurrentVersion";
+    public const string dataDirectoryEnvVar = $"{clickOncePrefix}DataDirectory";
+    public const string isFirstRunEnvVar = $"{clickOncePrefix}IsFirstRun";
+    public const string timeOfLastUpdateCheckEnvVar = $"{clickOncePrefix}TimeOfLastUpdateCheck";
+    public const string updatedApplicationFullNameEnvVar = $"{clickOncePrefix}UpdatedApplicationFullName";
+    public const string updatedVersionEnvVar = $"{clickOncePrefix}UpdatedVersion";
+    public const string updateLocationEnvVar = $"{clickOncePrefix}UpdateLocation";
+    public const string launcherVersionEnvVar = $"{clickOncePrefix}LauncherVersion";
+}
